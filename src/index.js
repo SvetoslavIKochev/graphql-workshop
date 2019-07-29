68ea6271fa2c70ab49958a2011666670e85f3f3b
SvetoslavIKochev <svetoslav.kochev@accedia.com>
2019-07-29 10:47:37 +0300

    demo 5 mutation - crate, delete and update user

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import { GraphQLServer, PubSub } from 'graphql-yoga'
 import db from './db'
 import Query from './resolvers/Query'
-import Mutation from './resolvers/Mutation'
-import Subscription from './resolvers/Subscription'
+// import Mutation from './resolvers/Mutation'
 import User from './resolvers/User'
 import Post from './resolvers/Post'
 import Comment from './resolvers/Comment'
@@ -13,8 +12,7 @@ const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers: {
         Query,
-        Mutation,
-        Subscription,
+        // Mutation,
         User,
         Post,
         Comment
