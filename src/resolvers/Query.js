e080e3cda6cb79ad07cbcc29863d49f04d0dda41
SvetoslavIKochev <svetoslav.kochev@accedia.com>
2019-08-26 09:27:55 +0300

    DEMO-2

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,28 +1,20 @@
 const Query = {
-    // users(parent, args, { db }, info) {
-    //     if (!args.query) {
-    //         return db.users
-    //     }
-    //
-    //     return db.users.filter((user) => {
-    //         return user.name.toLowerCase().includes(args.query.toLowerCase())
-    //     })
-    // },
-    me() {
-        return {
-            id: '123098',
-            name: 'Mike',
-            email: 'mike@example.com'
-        }
-    },
-    post() {
-        return {
-            id: '092',
-            title: 'GraphQL 101',
-            body: '',
-            published: false
-        }
+
+  me() {
+    return {
+      id: '123098',
+      name: 'Mike',
+      email: 'mike@example.com'
     }
+  },
+  post() {
+    return {
+      id: '092',
+      title: 'GraphQL 101',
+      body: '',
+      published: false
+    }
+  }
 };
 
 export default Query
