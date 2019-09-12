f3d5784bed117a58e4b8e5dd7e3f080dfb88f6a9
Stefan Stefanov <stefan.stefanov@accedia.com>
2019-09-12 09:14:44 +0300

    Update author and post field references

--- src/db.js
+++ src/db.js
@@ -18,41 +18,41 @@ const posts = [{
     title: 'GraphQL 101',
     body: 'This is how to use GraphQL...',
     published: true,
-    author: '1'
+    authorId: '1'
 }, {
     id: '11',
     title: 'GraphQL 201',
     body: 'This is an advanced GraphQL post...',
     published: false,
-    author: '1'
+    authorId: '1'
 }, {
     id: '12',
     title: 'Programming Music',
     body: '',
     published: true,
-    author: '2'
+    authorId: '2'
 }]
 
 const comments = [{
     id: '102',
     text: 'This worked well for me. Thanks!',
-    author: '3',
-    post: '10'
+    authorId: '3',
+    postId: '10'
 }, {
     id: '103',
     text: 'Glad you enjoyed it.',
-    author: '1',
-    post: '10'
+    authorId: '1',
+    postId: '10'
 }, {
     id: '104',
     text: 'This did no work.',
-    author: '2',
-    post: '11'
+    authorId: '2',
+    postId: '11'
 }, {
     id: '105',
     text: 'Nevermind. I got it to work.',
-    author: '1',
-    post: '12'
+    authorId: '1',
+    postId: '12'
 }]
 
 const db = {
